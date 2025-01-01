c0ecf901d20e9b9e0a82dfb71bfaeb7e7add9d95
Helic Leung <yoursun0@gmail.com>
2025-01-01 17:45:56 +0800

    fix missing BGG ID

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -23,10 +23,10 @@ $gameinfos = [
     'publisher_bgg_id' => 1234,
 
     // Board game geek ID of the game
-    'bgg_id' => 0,
+    'bgg_id' => 2389,
 
     // Players configuration that can be played (ex: 2 to 4 players)
-    'players' => [2, 3, 4],
+    'players' => [1, 2, 3, 4],
 
     // Suggest players to play with this number of players. Must be null if there is no such advice, or if there is only one possible player configuration.
     // NB: the automatic lobby will try first the lowest number of players if this is not specified. So you _have to_ specify this parameter if the lowest player number is not compatible with the default options.
