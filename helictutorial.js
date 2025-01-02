b313ce143c6d44533627189e33a192be2ba64ddf
Helic Leung <yoursun0@gmail.com>
2025-01-02 09:27:41 +0800

    chessboard setup

--- helictutorial.js
+++ helictutorial.js
@@ -50,11 +50,21 @@ function (dojo, declare) {
 
             // Example to add a div on the game area
             document.getElementById('game_play_area').insertAdjacentHTML('beforeend', `
-                <div id="player-tables"></div>
+                <div id="board"></div>
             `);
             
             // TODO: Set up your game interface here, according to "gamedatas"
-            
+            const board = document.getElementById('board');
+            const hor_scale = 64.8;
+            const ver_scale = 64.4;
+            for (let x=1; x<=8; x++) {
+                for (let y=1; y<=8; y++) {
+                    const left = Math.round((x - 1) * hor_scale + 10);
+                    const top = Math.round((y - 1) * ver_scale + 7);
+                    // we use afterbegin to make sure squares are placed before discs
+                    board.insertAdjacentHTML(`afterbegin`, `<div id="square_${x}_${y}" class="square" style="left: ${left}px; top: ${top}px;"></div>`);
+                }
+            }
  
             // Setup game notifications to handle (see "setupNotifications" method below)
             this.setupNotifications();
