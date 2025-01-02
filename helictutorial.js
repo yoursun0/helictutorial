04f19d4266301ddf689b17b5d6df7da217fb1706
Helic Leung <yoursun0@gmail.com>
2025-01-02 12:20:25 +0800

    game rules implement

--- helictutorial.js
+++ helictutorial.js
@@ -10,28 +10,27 @@
  * helictutorial.js
  *
  * HelicTutorial user interface script
- * 
+ *
  * In this file, you are describing the logic of your user interface, in Javascript language.
  *
  */
 
 define([
-    "dojo","dojo/_base/declare",
-    "ebg/core/gamegui",
-    "ebg/counter"
-],
-function (dojo, declare) {
-    return declare("bgagame.helictutorial", ebg.core.gamegui, {
-        constructor: function(){
-            console.log('helictutorial constructor');
-              
-            // Here, you can init the global variables of your user interface
-            // Example:
-            // this.myGlobalValue = 0;
+  "dojo",
+  "dojo/_base/declare",
+  "ebg/core/gamegui",
+  "ebg/counter",
+], function (dojo, declare) {
+  return declare("bgagame.helictutorial", ebg.core.gamegui, {
+    constructor: function () {
+      console.log("helictutorial constructor");
 
-        },
-        
-        /*
+      // Here, you can init the global variables of your user interface
+      // Example:
+      // this.myGlobalValue = 0;
+    },
+
+    /*
             setup:
             
             This method must set up the game user interface according to current game situation specified
@@ -43,60 +42,67 @@ function (dojo, declare) {
             
             "gamedatas" argument contains all datas retrieved by your "getAllDatas" PHP method.
         */
-        
-        setup: function( gamedatas )
-        {
-            console.log( "Starting game setup" );
 
-            // Example to add a div on the game area
-            document.getElementById('game_play_area').insertAdjacentHTML('beforeend', `
+    setup: function (gamedatas) {
+      console.log("Starting game setup");
+
+      // Example to add a div on the game area
+      document.getElementById("game_play_area").insertAdjacentHTML(
+        "beforeend",
+        `
                 <div id="board">
                     <div id="discs">
                     </div>
                 </div>
-            `);
-            
-            // TODO: Set up your game interface here, according to "gamedatas"
-            const board = document.getElementById('board');
-            const hor_scale = 64.8;
-            const ver_scale = 64.4;
-            for (let x=1; x<=8; x++) {
-                for (let y=1; y<=8; y++) {
-                    const left = Math.round((x - 1) * hor_scale + 10);
-                    const top = Math.round((y - 1) * ver_scale + 7);
-                    // we use afterbegin to make sure squares are placed before discs
-                    board.insertAdjacentHTML(`afterbegin`, `<div id="square_${x}_${y}" class="square" style="left: ${left}px; top: ${top}px;"></div>`);
-                }
-            }
+            `
+      );
 
-            for (var i in gamedatas.board){
-                var square = gamedatas.board[i];
-                if (square.player !== null){
-                    this.addDiscOnBoard( square.x, square.y, square.player);
-                }
-            }
- 
-            // Setup game notifications to handle (see "setupNotifications" method below)
-            this.setupNotifications();
+      // TODO: Set up your game interface here, according to "gamedatas"
+      const board = document.getElementById("board");
+      const hor_scale = 64.8;
+      const ver_scale = 64.4;
+      for (let x = 1; x <= 8; x++) {
+        for (let y = 1; y <= 8; y++) {
+          const left = Math.round((x - 1) * hor_scale + 10);
+          const top = Math.round((y - 1) * ver_scale + 7);
+          // we use afterbegin to make sure squares are placed before discs
+          board.insertAdjacentHTML(
+            `afterbegin`,
+            `<div id="square_${x}_${y}" class="square" style="left: ${left}px; top: ${top}px;"></div>`
+          );
+        }
+      }
 
-            console.log( "Ending game setup" );
-        },
-       
+      for (var i in gamedatas.board) {
+        var square = gamedatas.board[i];
+        if (square.player !== null) {
+          this.addDiscOnBoard(square.x, square.y, square.player);
+        }
+      }
 
-        ///////////////////////////////////////////////////
-        //// Game & client states
-        
-        // onEnteringState: this method is called each time we are entering into a new game state.
-        //                  You can use this method to perform some user interface changes at this moment.
-        //
-        onEnteringState: function( stateName, args )
-        {
-            console.log( 'Entering state: '+stateName );
-            
-            switch( stateName )
-            {
-            
-            /* Example:
+      document
+        .querySelectorAll(".square")
+        .forEach((square) =>
+          square.addEventListener("click", (e) => this.onPlayDisc(e))
+        );
+
+      // Setup game notifications to handle (see "setupNotifications" method below)
+      this.setupNotifications();
+
+      console.log("Ending game setup");
+    },
+
+    ///////////////////////////////////////////////////
+    //// Game & client states
+
+    // onEnteringState: this method is called each time we are entering into a new game state.
+    //                  You can use this method to perform some user interface changes at this moment.
+    //
+    onEnteringState: function (stateName, args) {
+      console.log("Entering state: " + stateName);
+
+      switch (stateName) {
+        /* Example:
             
             case 'myGameState':
             
@@ -105,25 +111,21 @@ function (dojo, declare) {
                 
                 break;
            */
-           
-           
-            case 'playerTurn':
-                this.updatePossibleMoves( args.args.possibleMoves );
-                break;
-            }
-        },
 
-        // onLeavingState: this method is called each time we are leaving a game state.
-        //                 You can use this method to perform some user interface changes at this moment.
-        //
-        onLeavingState: function( stateName )
-        {
-            console.log( 'Leaving state: '+stateName );
-            
-            switch( stateName )
-            {
-            
-            /* Example:
+        case "playerTurn":
+          this.updatePossibleMoves(args.args.possibleMoves);
+          break;
+      }
+    },
+
+    // onLeavingState: this method is called each time we are leaving a game state.
+    //                 You can use this method to perform some user interface changes at this moment.
+    //
+    onLeavingState: function (stateName) {
+      console.log("Leaving state: " + stateName);
+
+      switch (stateName) {
+        /* Example:
             
             case 'myGameState':
             
@@ -132,80 +134,66 @@ function (dojo, declare) {
                 
                 break;
            */
-           
-           
-            case 'dummy':
-                break;
-            }               
-        }, 
 
-        // onUpdateActionButtons: in this method you can manage "action buttons" that are displayed in the
-        //                        action status bar (ie: the HTML links in the status bar).
-        //        
-        onUpdateActionButtons: function( stateName, args )
-        {
-            console.log( 'onUpdateActionButtons: '+stateName, args );
-                      
-            if( this.isCurrentPlayerActive() )
-            {            
-                switch( stateName )
-                {
-                 case 'playerTurn':    
-                    const playableCardsIds = args.playableCardsIds; // returned by the argPlayerTurn
+        case "dummy":
+          break;
+      }
+    },
 
-                    // Add test action buttons in the action status bar, simulating a card click:
-                    playableCardsIds.forEach(
-                        cardId => this.addActionButton(`actPlayCard${cardId}-btn`, _('Play card with id ${card_id}').replace('${card_id}', cardId), () => this.onCardClick(cardId))
-                    ); 
+    // onUpdateActionButtons: in this method you can manage "action buttons" that are displayed in the
+    //                        action status bar (ie: the HTML links in the status bar).
+    //
+    onUpdateActionButtons: function (stateName, args) {
+      console.log("onUpdateActionButtons: " + stateName, args);
+    },
 
-                    this.addActionButton('actPass-btn', _('Pass'), () => this.bgaPerformAction("actPass"), null, null, 'gray'); 
-                    break;
-                }
-            }
-        },        
+    ///////////////////////////////////////////////////
+    //// Utility methods
 
-        ///////////////////////////////////////////////////
-        //// Utility methods
-        
-        /*
+    /*
         
             Here, you can defines some utility methods that you can use everywhere in your javascript
             script.
         
         */
 
-        addDiscOnBoard: async function( x, y, player )
-        {
-            const color = this.gamedatas.players[ player ].color;
-                
-            document.getElementById('discs').insertAdjacentHTML('beforeend', `<div class="disc" data-color="${color}" id="disc_${x}${y}"></div>`);
-                
-            this.placeOnObject( `disc_${x}${y}`, 'overall_player_board_'+player );
-                
-            const anim = this.slideToObject( `disc_${x}${y}`, 'square_'+x+'_'+y );
-            await this.bgaPlayDojoAnimation(anim);
-        },
-        
-        updatePossibleMoves: function( possibleMoves )
-        {
-            // Remove current possible moves
-            document.querySelectorAll('.possibleMove').forEach(div => div.classList.remove('possibleMove'));
-        
-            for( var x in possibleMoves )
-            {
-                for( var y in possibleMoves[ x ] )
-                {
-                    // x,y is a possible move
-                    document.getElementById(`square_${x}_${y}`).classList.add('possibleMove');
-                }            
-            }
-                        
-            this.addTooltipToClass( 'possibleMove', '', _('Place a disc here') );
-        },
-        ///////////////////////////////////////////////////
-        //// Player's action
-        
-        /*
+    addDiscOnBoard: async function (x, y, player) {
+      const color = this.gamedatas.players[player].color;
+
+      document
+        .getElementById("discs")
+        .insertAdjacentHTML(
+          "beforeend",
+          `<div class="disc" data-color="${color}" id="disc_${x}${y}"></div>`
+        );
+
+      this.placeOnObject(`disc_${x}${y}`, "overall_player_board_" + player);
+
+      const anim = this.slideToObject(`disc_${x}${y}`, "square_" + x + "_" + y);
+      await this.bgaPlayDojoAnimation(anim);
+    },
+
+    updatePossibleMoves: function (possibleMoves) {
+      // Remove current possible moves
+      document
+        .querySelectorAll(".possibleMove")
+        .forEach((div) => div.classList.remove("possibleMove"));
+
+      for (var x in possibleMoves) {
+        for (var y in possibleMoves[x]) {
+          // x,y is a possible move
+          document
+            .getElementById(`square_${x}_${y}`)
+            .classList.add("possibleMove");
+        }
+      }
+
+      this.addTooltipToClass("possibleMove", "", _("Place a disc here"));
+    },
+    ///////////////////////////////////////////////////
+    //// Player's action
+
+    /*
         
             Here, you are defining methods to handle player's action (ex: results of mouse click on 
             game objects).
@@ -215,26 +203,39 @@ function (dojo, declare) {
             _ make a call to the game server
         
         */
-        
-        // Example:
-        
-        onCardClick: function( card_id )
-        {
-            console.log( 'onCardClick', card_id );
 
-            this.bgaPerformAction("actPlayCard", { 
-                card_id,
-            }).then(() =>  {                
-                // What to do after the server call if it succeeded
-                // (most of the time, nothing, as the game will react to notifs / change of state instead)
-            });        
-        },    
+    // Example:
 
-        
-        ///////////////////////////////////////////////////
-        //// Reaction to cometD notifications
+    onPlayDisc: function (evt) {
+      // Stop this event propagation
+      evt.preventDefault();
+      evt.stopPropagation();
 
-        /*
+      // Get the cliqued square x and y
+      // Note: square id format is "square_X_Y"
+      var coords = evt.currentTarget.id.split("_");
+      var x = coords[1];
+      var y = coords[2];
+
+      if (
+        !document
+          .getElementById(`square_${x}_${y}`)
+          .classList.contains("possibleMove")
+      ) {
+        // This is not a possible move => the click does nothing
+        return;
+      }
+
+      this.bgaPerformAction("actPlayDisc", {
+        x: x,
+        y: y,
+      });
+    },
+
+    ///////////////////////////////////////////////////
+    //// Reaction to cometD notifications
+
+    /*
             setupNotifications:
             
             In this method, you associate each of your game notifications with your local method to handle it.
@@ -243,26 +244,25 @@ function (dojo, declare) {
                   your helictutorial.game.php file.
         
         */
-        setupNotifications: function()
-        {
-            console.log( 'notifications subscriptions setup' );
-            
-            // TODO: here, associate your game notifications with local methods
-            
-            // Example 1: standard notification handling
-            // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
-            
-            // Example 2: standard notification handling + tell the user interface to wait
-            //            during 3 seconds after calling the method in order to let the players
-            //            see what is happening in the game.
-            // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
-            // this.notifqueue.setSynchronous( 'cardPlayed', 3000 );
-            // 
-        },  
-        
-        // TODO: from this point and below, you can write your game notifications handling methods
-        
-        /*
+    setupNotifications: function () {
+      console.log("notifications subscriptions setup");
+
+      // TODO: here, associate your game notifications with local methods
+
+      // Example 1: standard notification handling
+      // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
+
+      // Example 2: standard notification handling + tell the user interface to wait
+      //            during 3 seconds after calling the method in order to let the players
+      //            see what is happening in the game.
+      // dojo.subscribe( 'cardPlayed', this, "notif_cardPlayed" );
+      // this.notifqueue.setSynchronous( 'cardPlayed', 3000 );
+      //
+    },
+
+    // TODO: from this point and below, you can write your game notifications handling methods
+
+    /*
         Example:
         
         notif_cardPlayed: function( notif )
@@ -276,5 +276,5 @@ function (dojo, declare) {
         },    
         
         */
-   });             
+  });
 });
--- modules/php/Game.php
+++ modules/php/Game.php
@@ -122,7 +122,7 @@ class Game extends \Table
         // Get some values from the current game situation from the database.
 
         return [
-            'possibleMoves' => $this->getPossibleMoves( intval($this->getActivePlayerId()) )
+            'possibleMoves' => $this->getPossibleMoves(intval($this->getActivePlayerId()))
         ];
     }
 
@@ -150,17 +150,45 @@ class Game extends \Table
      */
     public function stNextPlayer(): void
     {
-        // Retrieve the active player ID.
-        $player_id = (int)$this->getActivePlayerId();
+        // Active next player
+        $player_id = intval($this->activeNextPlayer());
 
-        // Give some extra time to the active player when he completed an action
-        $this->giveExtraTime($player_id);
+        // Check if both player has at least 1 discs, and if there are free squares to play
+        $player_to_discs = $this->getCollectionFromDb("SELECT board_player, COUNT( board_x )
+                                               FROM board
+                                               GROUP BY board_player", true);
 
-        $this->activeNextPlayer();
+        if (! isset($player_to_discs[null])) {
+            // Index 0 has not been set => there's no more free place on the board !
+            // => end of the game
+            $this->gamestate->nextState('endGame');
+            return;
+        } else if (! isset($player_to_discs[$player_id])) {
+            // Active player has no more disc on the board => he looses immediately
+            $this->gamestate->nextState('endGame');
+            return;
+        }
 
-        // Go to another gamestate
-        // Here, we would detect if the game is over, and in this case use "endGame" transition instead 
-        $this->gamestate->nextState("nextPlayer");
+        // Can this player play?
+
+        $possibleMoves = $this->getPossibleMoves($player_id);
+        if (count($possibleMoves) == 0) {
+
+            // This player can't play
+            // Can his opponent play ?
+            $opponent_id = (int)$this->getUniqueValueFromDb("SELECT player_id FROM player WHERE player_id!='$player_id' ");
+            if (count($this->getPossibleMoves($opponent_id)) == 0) {
+                // Nobody can move => end of the game
+                $this->gamestate->nextState('endGame');
+            } else {
+                // => pass his turn
+                $this->gamestate->nextState('cantPlay');
+            }
+        } else {
+            // This player can play. Give him some extra time
+            $this->giveExtraTime($player_id);
+            $this->gamestate->nextState('nextTurn');
+        }
     }
 
     /**
@@ -345,62 +373,60 @@ class Game extends \Table
         throw new \feException("Zombie mode not supported at this game state: \"{$state_name}\".");
     }
 
-        //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
     //////////// Utility functions    (functions used everywhere)
     ////////////
-    
+
     // Get the list of returned disc when "player" we play at this place ("x", "y"),
     //  or a void array if no disc is returned (invalid move)
-    function getTurnedOverDiscs( $x, $y, $player, $board )
+    function getTurnedOverDiscs($x, $y, $player, $board)
     {
         $turnedOverDiscs = array();
 
-        if( $board[ $x ][ $y ] === null ) // If there is already a disc on this place, this can't be a valid move
+        if ($board[$x][$y] === null) // If there is already a disc on this place, this can't be a valid move
         {
             // For each directions...
             $directions = array(
-                array( -1,-1 ), array( -1,0 ), array( -1, 1 ), array( 0, -1),
-                array( 0,1 ), array( 1,-1), array( 1,0 ), array( 1, 1 )
+                array(-1, -1),
+                array(-1, 0),
+                array(-1, 1),
+                array(0, -1),
+                array(0, 1),
+                array(1, -1),
+                array(1, 0),
+                array(1, 1)
             );
 
-            foreach( $directions as $direction )
-            {
+            foreach ($directions as $direction) {
                 // Starting from the square we want to place a disc...
                 $current_x = $x;
                 $current_y = $y;
                 $bContinue = true;
                 $mayBeTurnedOver = array();
 
-                while( $bContinue )
-                {
+                while ($bContinue) {
                     // Go to the next square in this direction
                     $current_x += $direction[0];
                     $current_y += $direction[1];
 
-                    if( $current_x<1 || $current_x>8 || $current_y<1 || $current_y>8 )
+                    if ($current_x < 1 || $current_x > 8 || $current_y < 1 || $current_y > 8)
                         $bContinue = false; // Out of the board => stop here for this direction
-                    else if( $board[ $current_x ][ $current_y ] === null )
+                    else if ($board[$current_x][$current_y] === null)
                         $bContinue = false; // An empty square => stop here for this direction
-                    else if( $board[ $current_x ][ $current_y ] != $player )
-                    {
+                    else if ($board[$current_x][$current_y] != $player) {
                         // There is a disc from our opponent on this square
                         // => add it to the list of the "may be turned over", and continue on this direction
-                        $mayBeTurnedOver[] = array( 'x' => $current_x, 'y' => $current_y );
-                    }
-                    else if( $board[ $current_x ][ $current_y ] == $player )
-                    {
+                        $mayBeTurnedOver[] = array('x' => $current_x, 'y' => $current_y);
+                    } else if ($board[$current_x][$current_y] == $player) {
                         // This is one of our disc
 
-                        if( count( $mayBeTurnedOver ) == 0 )
-                        {
+                        if (count($mayBeTurnedOver) == 0) {
                             // There is no disc to be turned over between our 2 discs => stop here for this direction
                             $bContinue = false;
-                        }
-                        else
-                        {
+                        } else {
                             // We found some disc to be turned over between our 2 discs
                             // => add them to the result and stop here for this direction
-                            $turnedOverDiscs = array_merge( $turnedOverDiscs, $mayBeTurnedOver );
+                            $turnedOverDiscs = array_merge($turnedOverDiscs, $mayBeTurnedOver);
                             $bContinue = false;
                         }
                     }
@@ -414,30 +440,25 @@ class Game extends \Table
     // Get the complete board with a double associative array
     function getBoard()
     {
-        return self::getDoubleKeyCollectionFromDB( "SELECT board_x x, board_y y, board_player player
-                                                       FROM board", true );
+        return self::getDoubleKeyCollectionFromDB("SELECT board_x x, board_y y, board_player player
+                                                       FROM board", true);
     }
 
     // Get the list of possible moves (x => y => true)
-    function getPossibleMoves( $player_id )
+    function getPossibleMoves($player_id)
     {
         $result = array();
 
         $board = self::getBoard();
 
-        for( $x=1; $x<=8; $x++ )
-        {
-            for( $y=1; $y<=8; $y++ )
-            {
-                $returned = self::getTurnedOverDiscs( $x, $y, $player_id, $board );
-                if( count( $returned ) == 0 )
-                {
+        for ($x = 1; $x <= 8; $x++) {
+            for ($y = 1; $y <= 8; $y++) {
+                $returned = self::getTurnedOverDiscs($x, $y, $player_id, $board);
+                if (count($returned) == 0) {
                     // No discs returned => not a possible move
-                }
-                else
-                {
+                } else {
                     // Okay => set this coordinate to "true"
-                    if( ! isset( $result[$x] ) )
+                    if (! isset($result[$x]))
                         $result[$x] = array();
 
                     $result[$x][$y] = true;
@@ -446,5 +467,65 @@ class Game extends \Table
         }
 
         return $result;
+    }
+
+    function actPlayDisc(int $x, int $y)
+    {
+        $player_id = intval($this->getActivePlayerId());
+
+        // Now, check if this is a possible move
+        $board = $this->getBoard();
+        $turnedOverDiscs = $this->getTurnedOverDiscs($x, $y, $player_id, $board);
+
+        if (count($turnedOverDiscs) > 0) {
+            // This move is possible!
+            // Let's place a disc at x,y and return all "$returned" discs to the active player
+
+            $sql = "UPDATE board SET board_player='$player_id'
+                    WHERE ( board_x, board_y) IN ( ";
+
+            foreach ($turnedOverDiscs as $turnedOver) {
+                $sql .= "('" . $turnedOver['x'] . "','" . $turnedOver['y'] . "'),";
+            }
+            $sql .= "('$x','$y') ) ";
+
+            $this->DbQuery($sql);
+            // Update scores according to the number of disc on board
+            $sql = "UPDATE player
+             SET player_score = (
+             SELECT COUNT( board_x ) FROM board WHERE board_player=player_id
+             )";
+            $this->DbQuery($sql);
+
+            // Statistics
+            $this->incStat(count($turnedOverDiscs), "turnedOver", $player_id);
+            if (($x == 1 && $y == 1) || ($x == 8 && $y == 1) || ($x == 1 && $y == 8) || ($x == 8 && $y == 8))
+                $this->incStat(1, 'discPlayedOnCorner', $player_id);
+            else if ($x == 1 || $x == 8 || $y == 1 || $y == 8)
+                $this->incStat(1, 'discPlayedOnBorder', $player_id);
+            else if ($x >= 3 && $x <= 6 && $y >= 3 && $y <= 6)
+                $this->incStat(1, 'discPlayedOnCenter', $player_id);
+            // Notify
+            $this->notifyAllPlayers("playDisc", clienttranslate('${player_name} plays a disc and turns over ${returned_nbr} disc(s)'), array(
+                'player_id' => $player_id,
+                'player_name' => $this->getActivePlayerName(),
+                'returned_nbr' => count($turnedOverDiscs),
+                'x' => $x,
+                'y' => $y
+            ));
+
+            $this->notifyAllPlayers("turnOverDiscs", '', array(
+                'player_id' => $player_id,
+                'turnedOver' => $turnedOverDiscs
+            ));
+
+            $newScores = $this->getCollectionFromDb("SELECT player_id, player_score FROM player", true);
+            $this->notifyAllPlayers("newScores", "", array(
+                "scores" => $newScores
+            ));
+            // Then, go to the next state
+            $this->gamestate->nextState('playDisc');
+        } else
+            throw new \BgaSystemException("Impossible move");
     }
 }
